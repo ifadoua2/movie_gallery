5c6c2dc970ff2d494663a13069488950a8187743
jaume vidal <jaumevibu@gmail.com>
2023-02-01 15:52:37 +0100

    even more fixing missing files :S

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { MovieCard } from "./components/MovieCard/MovieCard";
+import { Movie } from "./model/Movie";
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <MovieCard movieProp={new Movie('1', 'Zoy un titulo', 'Zoy un un director', '2023', '')} />
+  </div>;
 }
 
 export default App;
