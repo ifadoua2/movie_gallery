14905ffbe6244658419aeef6cc1041195b8dd609
Pere1J <pere1josep@gmail.com>
2023-02-02 16:19:03 +0100

    added movie list and api and useeffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { MovieCard } from "./components/MovieCard/MovieCard";
 import { Movie } from "./model/Movie";
-
+import { MovieList } from "./components/MovieList/MovieList";
 
 function App() {
-  return <div className="App">
-    <MovieCard movieProp={new Movie('1', 'Zoy un titulo', 'Zoy un un director', '2023', '')} />
-  </div>;
+  return (
+    <div className="App">
+      {/*     <MovieCard movieProp={new Movie('1', 'Zoy un titulo', 'Zoy un un director', '2023', '')} />
+       */}
+      <MovieList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Movie } from "../../model/Movie";
 
 export const MovieList = () => {
-  return <div>MovieList</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    /*  setMovies(moviesDataBase); */
+    fetch("https://63dbb6b2c45e08a0434c15f9.mockapi.io/v1/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li>
+          {movie.title}
+          <img src={movie.img} alt={movie.title + "poster"}></img>
+        </li>
+      ))}
+    </div>
+  );
 };
