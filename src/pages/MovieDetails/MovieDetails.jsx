2961a9130e9d5865aed20467dd8ef1ae448cc256
Pere1J <pere1josep@gmail.com>
2023-02-14 11:36:52 +0100

    added spinner on movie details

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -3,8 +3,10 @@ import { useParams } from "react-router-dom";
 import styles from "./MovieDetails.module.css";
 import movieApiService from "../../apiServices/movieApiService.js";
 import { AiFillStar } from "react-icons/ai";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export const MovieDetails = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
   const [movie, setMovie] = useState();
 
@@ -21,47 +23,63 @@ export const MovieDetails = () => {
   };
 
   useEffect(() => {
-    movieApiService.getMovieById(id).then((data) => setMovie(data));
+    movieApiService.getMovieById(id).then((data) => {
+      setMovie(data);
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <div>
-      {movie === undefined ? (
-        ""
+      {isLoading ? (
+        <ClipLoader
+          className={styles.clipLoader}
+          loading={isLoading}
+          size={300}
+          color={"red"}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
       ) : (
-        <div className={styles.detailsContainer}>
-          <div className={styles.contenedorImagen}>
-            <img src={movie.img} alt={movie.title} />
-          </div>
-          <div className={styles.contenedorDatosCortos}>
-            <div className={styles.contenedorFavoritos}>
-              <h3 className={styles.tituloDatos}>Favorito</h3>
-              <AiFillStar
-                onClick={toogleIsFavourite}
-                className={`${styles.iconFavourite} ${
-                  movie.isFavourite ? styles.iconFavouriteActive : ""
-                }`}
-              />
-            </div>
+        <>
+          {movie === undefined ? (
+            ""
+          ) : (
+            <div className={styles.detailsContainer}>
+              <div className={styles.contenedorImagen}>
+                <img src={movie.img} alt={movie.title} />
+              </div>
+              <div className={styles.contenedorDatosCortos}>
+                <div className={styles.contenedorFavoritos}>
+                  <h3 className={styles.tituloDatos}>Favorito</h3>
+                  <AiFillStar
+                    onClick={toogleIsFavourite}
+                    className={`${styles.iconFavourite} ${
+                      movie.isFavourite ? styles.iconFavouriteActive : ""
+                    }`}
+                  />
+                </div>
 
-            <div>
-              <h3 className={styles.tituloDatos}>Titulo</h3>
-              <p>{movie.title}</p>
+                <div>
+                  <h3 className={styles.tituloDatos}>Titulo</h3>
+                  <p>{movie.title}</p>
+                </div>
+                <div>
+                  <h3 className={styles.tituloDatos}>Director</h3>
+                  <p>{movie.director}</p>
+                </div>
+                <div>
+                  <h3 className={styles.tituloDatos}>Año</h3>
+                  <p>{movie.year}</p>
+                </div>
+                <div className={styles.contenedorSinopsis}>
+                  <h3 className={styles.tituloDatos}>Sinopsis</h3>
+                  <p>{movie.sinopsis}</p>
+                </div>
+              </div>
             </div>
-            <div>
-              <h3 className={styles.tituloDatos}>Director</h3>
-              <p>{movie.director}</p>
-            </div>
-            <div>
-              <h3 className={styles.tituloDatos}>Año</h3>
-              <p>{movie.year}</p>
-            </div>
-            <div className={styles.contenedorSinopsis}>
-              <h3 className={styles.tituloDatos}>Sinopsis</h3>
-              <p>{movie.sinopsis}</p>
-            </div>
-          </div>
-        </div>
+          )}
+        </>
       )}
     </div>
   );
