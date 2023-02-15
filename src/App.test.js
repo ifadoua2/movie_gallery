8486d1665daed2cdd510cba9a543f30605f6c7c8
jaume vidal <jaumevibu@gmail.com>
2023-02-15 14:46:32 +0100

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders movie gallery title in child navbar", () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = screen.getByText(/movie gallery/i);
   expect(linkElement).toBeInTheDocument();
 });
