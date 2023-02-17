ded9ad1419866a31480b0dfca4a748f4746901ff
jaume vidal <jaumevibu@gmail.com>
2023-02-17 07:49:05 +0100

    name changes

--- src/components/SimpleMiniComponentForTesting/SimpleMiniComponentForTesting.test.js
+++ src/components/SimpleMiniComponentForTesting/SimpleMiniComponentForTesting.test.js
@@ -1,10 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import SimpleMiniComponentForTesting from "./SimpleMiniComponentForTesting";
 
 test("when clicked on the rendered button it executes the handleClickFunction once", () => {
   const handleBtnClick = jest.fn();
-  render(<BrowserRouter><SimpleMiniComponentForTesting handleBtnClick={handleBtnClick} /></BrowserRouter>);
+  render(<SimpleMiniComponentForTesting handleBtnClick={handleBtnClick} />);
   fireEvent.click(screen.getByText(/Test me!/i))
   expect(handleBtnClick).toHaveBeenCalledTimes(1);
   expect(handleBtnClick).toHaveBeenCalled();
